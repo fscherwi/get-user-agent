353fe8abc149a879309d6ca04dd65387b531c864
fscherwi <Florian@ScherwitzlsiMac.home>
2015-09-10 15:53:45 +0200

    version preparations

--- test.js
+++ test.js
@@ -6,4 +6,4 @@ console.log(useragent.opera("2.12.388") + "\n");
 
 console.log("\n" + useragent.chrome_advanced(version="41.0.2228.0", webkit_version="537.36"));
 console.log(useragent.firefox_advanced(version="40.0", gecko_version="20100101"));
-console.log(useragent.opera_advanced(version="2.12.388", presto_version="2.12.388") + "\n");
+console.log(useragent.opera_advanced(version="12.6", presto_version="2.12.388") + "\n");
