332df942fb61de687008ebe82174d6e80941ce46
fscherwi <Florian@ScherwitzlsiMac.home>
2015-11-01 20:17:08 +0100

    also show os info

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ var chrome = useragent.chrome("41.0.2228.0"),
   opera = useragent.opera("12.6"),
   opera_advanced = useragent.opera_advanced(version = "12.6", presto_version = "2.12.388", opera_version = "9.80");
 
-console.log("\n%s: \t %s", r.parse(chrome).ua.toString(), chrome);
-console.log("%s: \t\t %s", r.parse(firefox).ua.toString(), firefox);
-console.log("%s: \t\t %s", r.parse(opera).ua.toString(), opera);
+console.log("\n%s (%s): \t %s", r.parse(chrome).ua.toString(), r.parse(chrome).os.toString(), chrome);
+console.log("%s \t (%s): \t %s", r.parse(firefox).ua.toString(), r.parse(firefox).os.toString(), firefox);
+console.log("%s \t (%s): \t %s", r.parse(opera).ua.toString(), r.parse(opera).os.toString(), opera);
 
-console.log("\n%s: \t %s", r.parse(chrome_advanced).ua.toString(), chrome_advanced);
-console.log("%s: \t\t %s", r.parse(firefox_advanced).ua.toString(), firefox_advanced);
-console.log("%s: \t\t %s\n", r.parse(opera_advanced).ua.toString(), opera_advanced);
+console.log("\n%s (%s): \t %s", r.parse(chrome_advanced).ua.toString(), r.parse(chrome_advanced).os.toString(), chrome_advanced);
+console.log("%s \t (%s): \t %s", r.parse(firefox_advanced).ua.toString(), r.parse(firefox_advanced).os.toString(), firefox_advanced);
+console.log("%s \t (%s): \t %s\n", r.parse(opera_advanced).ua.toString(), r.parse(opera_advanced).os.toString(), opera_advanced);
