368a8da6a1cb2e071ff7ec277e15ec8e1656332d
fscherwi <Florian@ScherwitzlsiMac.home>
2015-11-02 09:15:46 +0100

    replace " with '

--- main.js
+++ main.js
@@ -7,40 +7,40 @@ if (os.platform() === 'darwin') {
   arch = require('get-osx-version').get();
   platform = 'Macintosh; Intel Mac OS X ';
 } else if (os.platform() === 'win32') {
-  platform = "Windows";
+  platform = 'Windows';
   if (os.arch() === 'x64') {
-    arch = " ; Win64; x64";
+    arch = ' ; Win64; x64';
   } else if (os.arch() === 'arm') {
-    arch = "; ARM";
+    arch = '; ARM';
   }
 } else if (os.platform() === 'linux') {
   platform = 'Linux';
   if (os.arch() === 'x64') {
-    arch = " x86_64";
+    arch = ' x86_64';
   } else if (os.arch() === 'ia32') {
-    arch = "";
+    arch = '';
   } else if (os.arch() === 'arm') {
-    arch = "; ARM";
+    arch = '; ARM';
   }
 }
 osinfo = platform + arch;
 module.exports = {
   chrome: function chrome(version) {
-    return "Mozilla/5.0" + " (" + osinfo + ")" + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + version + " Safari/537.36";
+    return 'Mozilla/5.0' + ' (' + osinfo + ')' + ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' + version + ' Safari/537.36';
   },
   chrome_advanced: function chrome_advanced(version, webkit_version) {
-    return "Mozilla/5.0" + " (" + osinfo + ")" + " AppleWebKit/" + webkit_version + " (KHTML, like Gecko) Chrome/" + version + " Safari/" + webkit_version;
+    return 'Mozilla/5.0' + ' (' + osinfo + ')' + ' AppleWebKit/' + webkit_version + ' (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit_version;
   },
   firefox: function firefox(version) {
-    return "Mozilla/5.0" + " (" + osinfo + "; rv:" + version + ")" + " Gecko/20100101 Firefox/" + version;
+    return 'Mozilla/5.0' + ' (' + osinfo + '; rv:' + version + ')' + ' Gecko/20100101 Firefox/' + version;
   },
   firefox_advanced: function firefox_advanced(version, gecko_version) {
-    return "Mozilla/5.0" + " (" + osinfo + "; rv:" + version + ")" + " Gecko/" + gecko_version + " Firefox/" + version;
+    return 'Mozilla/5.0' + ' (' + osinfo + '; rv:' + version + ')' + ' Gecko/' + gecko_version + ' Firefox/' + version;
   },
   opera: function opera(version) {
-    return "Opera/9.80 (" + osinfo + " ) Presto/2.12.388 Version/" + version;
+    return 'Opera/9.80 (' + osinfo + ') Presto/2.12.388 Version/' + version;
   },
   opera_advanced: function opera_advanced(version, presto_version, opera_version) {
-    return "Opera/" + opera_version + " (" + osinfo + " ) Presto/" + presto_version + " Version/" + version;
+    return 'Opera/' + opera_version + ' (' + osinfo + ') Presto/' + presto_version + ' Version/' + version;
   }
 };
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 var useragent = require('./main.js');
 var r = require('ua-parser');
 
-var chrome = useragent.chrome("41.0.2228.0"),
-  chrome_advanced = useragent.chrome_advanced(version = "41.0.2228.0", webkit_version = "537.36"),
-  firefox = useragent.firefox("40.0"),
-  firefox_advanced = useragent.firefox_advanced(version = "40.0", gecko_version = "20100101"),
-  opera = useragent.opera("12.6"),
-  opera_advanced = useragent.opera_advanced(version = "12.6", presto_version = "2.12.388", opera_version = "9.80");
+var chrome = useragent.chrome('41.0.2228.0');
+var chrome_advanced = useragent.chrome_advanced(version = '41.0.2228.0', webkit_version = '537.36');
+var firefox = useragent.firefox('41.0');
+var firefox_advanced = useragent.firefox_advanced(version = '41.0', gecko_version = '20100101');
+var opera = useragent.opera('12.6');
+var opera_advanced = useragent.opera_advanced(version = '12.6', presto_version = '2.12.388', opera_version = '9.80');
 
-console.log("\n%s (%s): \t %s", r.parse(chrome).ua.toString(), r.parse(chrome).os.toString(), chrome);
-console.log("%s \t (%s): \t %s", r.parse(firefox).ua.toString(), r.parse(firefox).os.toString(), firefox);
-console.log("%s \t (%s): \t %s", r.parse(opera).ua.toString(), r.parse(opera).os.toString(), opera);
+console.log('\n%s (%s): \t %s', r.parse(chrome).ua.toString(), r.parse(chrome).os.toString(), chrome);
+console.log('%s \t (%s): \t %s', r.parse(firefox).ua.toString(), r.parse(firefox).os.toString(), firefox);
+console.log('%s \t (%s): \t %s', r.parse(opera).ua.toString(), r.parse(opera).os.toString(), opera);
 
-console.log("\n%s (%s): \t %s", r.parse(chrome_advanced).ua.toString(), r.parse(chrome_advanced).os.toString(), chrome_advanced);
-console.log("%s \t (%s): \t %s", r.parse(firefox_advanced).ua.toString(), r.parse(firefox_advanced).os.toString(), firefox_advanced);
-console.log("%s \t (%s): \t %s\n", r.parse(opera_advanced).ua.toString(), r.parse(opera_advanced).os.toString(), opera_advanced);
+console.log('\n%s (%s): \t %s', r.parse(chrome_advanced).ua.toString(), r.parse(chrome_advanced).os.toString(), chrome_advanced);
+console.log('%s \t (%s): \t %s', r.parse(firefox_advanced).ua.toString(), r.parse(firefox_advanced).os.toString(), firefox_advanced);
+console.log('%s \t (%s): \t %s\n', r.parse(opera_advanced).ua.toString(), r.parse(opera_advanced).os.toString(), opera_advanced);
