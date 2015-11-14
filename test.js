7106327f5064dde3c564314fa5a8c2f1eb0c7a1d
fscherwi <fscherwi@users.noreply.github.com>
2015-11-14 20:05:33 +0100

    remove tabs

--- test.js
+++ test.js
@@ -8,10 +8,10 @@ var firefox_advanced = useragent.firefox_advanced(version = '41.0', gecko_versio
 var opera = useragent.opera('12.6');
 var opera_advanced = useragent.opera_advanced(version = '12.6', presto_version = '2.12.388', opera_version = '9.80');
 
-console.log('\n%s (%s): \t %s', r.parse(chrome).ua.toString(), r.parse(chrome).os.toString(), chrome);
-console.log('%s \t (%s): \t %s', r.parse(firefox).ua.toString(), r.parse(firefox).os.toString(), firefox);
-console.log('%s \t (%s): \t %s', r.parse(opera).ua.toString(), r.parse(opera).os.toString(), opera);
+console.log('\n%s (%s): %s', r.parse(chrome).ua.toString(), r.parse(chrome).os.toString(), chrome);
+console.log('%s \t (%s): %s', r.parse(firefox).ua.toString(), r.parse(firefox).os.toString(), firefox);
+console.log('%s \t (%s): %s', r.parse(opera).ua.toString(), r.parse(opera).os.toString(), opera);
 
-console.log('\n%s (%s): \t %s', r.parse(chrome_advanced).ua.toString(), r.parse(chrome_advanced).os.toString(), chrome_advanced);
-console.log('%s \t (%s): \t %s', r.parse(firefox_advanced).ua.toString(), r.parse(firefox_advanced).os.toString(), firefox_advanced);
-console.log('%s \t (%s): \t %s\n', r.parse(opera_advanced).ua.toString(), r.parse(opera_advanced).os.toString(), opera_advanced);
+console.log('\n%s (%s): %s', r.parse(chrome_advanced).ua.toString(), r.parse(chrome_advanced).os.toString(), chrome_advanced);
+console.log('%s \t (%s): %s', r.parse(firefox_advanced).ua.toString(), r.parse(firefox_advanced).os.toString(), firefox_advanced);
+console.log('%s \t (%s): %s\n', r.parse(opera_advanced).ua.toString(), r.parse(opera_advanced).os.toString(), opera_advanced);
