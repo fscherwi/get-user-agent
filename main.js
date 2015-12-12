aaa946b8ca0b2a7955c5efea919828783b6ff675
fscherwi <Florian@ScherwitzlsiMac.home>
2015-12-12 16:49:00 +0100

    improvements

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var os = require('os');
-var agent_platform;
-var agent_arch;
-var os_platform = os.platform();
-var os_arch = os.arch();
 /* istanbul ignore next */
 function get_sys_info() {
+  var os = require('os');
+  var agent_platform;
+  var agent_arch;
+  var os_platform = os.platform();
+  var os_arch = os.arch();
   switch (os_platform) {
     case 'darwin':
       agent_platform = 'Macintosh; Intel Mac OS X ';
@@ -31,39 +31,33 @@ function get_sys_info() {
 }
 module.exports = {
   chrome: function chrome(version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Mozilla/5.0' + ' (' + sys_info + ')' + ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' + version + ' Safari/537.36';
   },
   chrome_advanced: function chrome_advanced(version, webkit_version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Mozilla/5.0' + ' (' + sys_info + ')' + ' AppleWebKit/' + webkit_version + ' (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit_version;
   },
   firefox: function firefox(version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Mozilla/5.0' + ' (' + sys_info + '; rv:' + version + ')' + ' Gecko/20100101 Firefox/' + version;
   },
   firefox_advanced: function firefox_advanced(version, gecko_version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Mozilla/5.0' + ' (' + sys_info + '; rv:' + version + ')' + ' Gecko/' + gecko_version + ' Firefox/' + version;
   },
   opera: function opera(version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Opera/9.80 (' + sys_info + ') Presto/2.12.388 Version/' + version;
   },
   opera_advanced: function opera_advanced(version, presto_version, opera_version, sys_info) {
-    if (sys_info === undefined) {
+    if (sys_info === undefined)
       sys_info = get_sys_info();
-    }
     return 'Opera/' + opera_version + ' (' + sys_info + ') Presto/' + presto_version + ' Version/' + version;
   }
 };
