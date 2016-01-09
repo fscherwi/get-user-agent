9cf4727ecf0380cd5ae9572e432477b462d73f7f
fscherwi <Florian@ScherwitzlsiMac.home>
2016-01-09 20:48:01 +0100

    improvements

--- main.js
+++ main.js
@@ -1,59 +1,51 @@
 /* istanbul ignore next */ /*jshint -W086 */
-function get_sys_info() {
-  var os = require('os');
-  var os_arch = os.arch();
-  switch (os.platform()) {
-    case 'darwin':
-      return 'Macintosh; Intel Mac OS X ' + require('get-osx-version').get();
-    case 'windows':
-      if (os_arch === 'x64') {
-        return 'Windows; Win64; x64';
-      } else if (os_arch === 'arm') {
-        return 'Windows; ARM';
-      } else {
-        return 'Windows';
-      }
-    case 'linux':
-      if (os_arch === 'x64') {
-        return 'Linux; x86_64';
-      } else if (os_arch === 'arm') {
-        return 'Linux; ARM';
-      } else {
-        return 'Linux';
-      }
-    default:
-      return;
+function get_sys_info(sys_info) {
+  if (sys_info === undefined) {
+    var os = require('os');
+    var os_arch = os.arch();
+    switch (os.platform()) {
+      case 'darwin':
+        return 'Macintosh; Intel Mac OS X ' + require('get-osx-version').get();
+      case 'windows':
+        if (os_arch === 'x64') {
+          return 'Windows; Win64; x64';
+        } else if (os_arch === 'arm') {
+          return 'Windows; ARM';
+        } else {
+          return 'Windows';
+        }
+      case 'linux':
+        if (os_arch === 'x64') {
+          return 'Linux; x86_64';
+        } else if (os_arch === 'arm') {
+          return 'Linux; ARM';
+        } else {
+          return 'Linux';
+        }
+      default:
+        return;
+    }
+  } else {
+    return sys_info;
   }
 }
 module.exports = {
   chrome: function chrome(version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Mozilla/5.0' + ' (' + sys_info + ')' + ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' + version + ' Safari/537.36';
+    return 'Mozilla/5.0' + ' (' + get_sys_info(sys_info) + ')' + ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/' + version + ' Safari/537.36';
   },
   chrome_advanced: function chrome_advanced(version, webkit_version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Mozilla/5.0' + ' (' + sys_info + ')' + ' AppleWebKit/' + webkit_version + ' (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit_version;
+    return 'Mozilla/5.0' + ' (' + get_sys_info(sys_info) + ')' + ' AppleWebKit/' + webkit_version + ' (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit_version;
   },
   firefox: function firefox(version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Mozilla/5.0' + ' (' + sys_info + '; rv:' + version + ')' + ' Gecko/20100101 Firefox/' + version;
+    return 'Mozilla/5.0' + ' (' + get_sys_info(sys_info) + '; rv:' + version + ')' + ' Gecko/20100101 Firefox/' + version;
   },
   firefox_advanced: function firefox_advanced(version, gecko_version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Mozilla/5.0' + ' (' + sys_info + '; rv:' + version + ')' + ' Gecko/' + gecko_version + ' Firefox/' + version;
+    return 'Mozilla/5.0' + ' (' + get_sys_info(sys_info) + '; rv:' + version + ')' + ' Gecko/' + gecko_version + ' Firefox/' + version;
   },
   opera: function opera(version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Opera/9.80 (' + sys_info + ') Presto/2.12.388 Version/' + version;
+    return 'Opera/9.80 (' + get_sys_info(sys_info) + ') Presto/2.12.388 Version/' + version;
   },
   opera_advanced: function opera_advanced(version, presto_version, opera_version, sys_info) {
-    if (sys_info === undefined)
-      sys_info = get_sys_info();
-    return 'Opera/' + opera_version + ' (' + sys_info + ') Presto/' + presto_version + ' Version/' + version;
+    return 'Opera/' + opera_version + ' (' + get_sys_info(sys_info) + ') Presto/' + presto_version + ' Version/' + version;
   }
 };
