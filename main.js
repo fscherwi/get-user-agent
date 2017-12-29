15995dad6d281eecbf4f5183bf20179ecf8cdb1c
fscherwi <fscherwi@outlook.com>
2017-12-29 20:47:05 +0100

    style

--- main.js
+++ main.js
@@ -37,25 +37,25 @@ module.exports = {
   chrome: function chrome(version, SysInfo) {
     return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + ")" + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + version + " Safari/537.36";
   },
-  chrome_advanced: function chrome_advanced(version, WebkitVersion, SysInfo) {
+  ChromeAdvanced: function ChromeAdvanced(version, WebkitVersion, SysInfo) {
     return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + ")" + " AppleWebKit/" + WebkitVersion + " (KHTML, like Gecko) Chrome/" + version + " Safari/" + WebkitVersion;
   },
   firefox: function firefox(version, SysInfo) {
     return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/20100101 Firefox/" + version;
   },
-  firefox_advanced: function firefox_advanced(version, GeckoVersion, SysInfo) {
+  FirefoxAdvanced: function FirefoxAdvanced(version, GeckoVersion, SysInfo) {
     return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/" + GeckoVersion + " Firefox/" + version;
   },
   opera: function opera(version, SysInfo) {
     return "Opera/9.80 (" + GetSysInfo(SysInfo) + ") Presto/2.12.388 Version/" + version;
   },
-  opera_advanced: function opera_advanced(version, PrestoVersion, OperaVersion, SysInfo) {
+  OperaAdvanced: function OperaAdvanced(version, PrestoVersion, OperaVersion, SysInfo) {
     return "Opera/" + OperaVersion + " (" + GetSysInfo(SysInfo) + ") Presto/" + PrestoVersion + " Version/" + version;
   },
   safari: function safari(version, SysInfo) {
     return "Mozilla/5.0 (" + GetSysInfo(SysInfo) + ") AppleWebKit/601.4.8 (KHTML, like Gecko) Version/" + version + " Safari/601.4.8";
   },
-  safari_advanced: function safari_advanced(version, WebkitBuildVersion, SafariBuildVersion, SysInfo) {
+  SafariAdvanced: function SafariAdvanced(version, WebkitBuildVersion, SafariBuildVersion, SysInfo) {
     return "Mozilla/5.0 (" + GetSysInfo(SysInfo) + ") AppleWebKit/" + WebkitBuildVersion + " (KHTML, like Gecko) Version/" + version + " Safari/" + SafariBuildVersion;
   }
 };
--- test.js
+++ test.js
@@ -2,13 +2,13 @@ var useragent = require("./main.js");
 var parser = require("ua-parser");
 var agents = [
   useragent.chrome("41.0.2228.0"),
-  useragent.chrome_advanced("41.0.2228.0", "537.36"),
+  useragent.ChromeAdvanced("41.0.2228.0", "537.36"),
   useragent.firefox("41.0"),
-  useragent.firefox_advanced("41.0", "20100101"),
+  useragent.FirefoxAdvanced("41.0", "20100101"),
   useragent.opera("12.6"),
-  useragent.opera_advanced("12.6", "2.12.388", "9.80"),
+  useragent.OperaAdvanced("12.6", "2.12.388", "9.80"),
   useragent.safari("10.0.3"),
-  useragent.safari_advanced("10.0.3", "602.4.8", "602.4.8")
+  useragent.SafariAdvanced("10.0.3", "602.4.8", "602.4.8")
 ];
 agents.forEach(function (UserAgent) {
   console.log("%s (%s):\t %s", parser.parse(UserAgent).ua.toString(), parser.parse(UserAgent).os.toString(), UserAgent);
