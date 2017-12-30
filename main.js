01bccac7222be6a27cb8256045389d970183cc0b
fscherwi <fscherwi@outlook.com>
2017-12-30 12:01:49 +0100

    style

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function GetSysInfo(SysInfo) { /* istanbul ignore next */
+function getSysInfo(SysInfo) { /* istanbul ignore next */
   if (SysInfo) {
     return SysInfo;
   } else {
@@ -33,27 +33,27 @@ function GetSysInfo(SysInfo) { /* istanbul ignore next */
 }
 module.exports = {
   chrome: function chrome(version, SysInfo) {
-    return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + ")" + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + version + " Safari/537.36";
+    return "Mozilla/5.0" + " (" + getSysInfo(SysInfo) + ")" + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + version + " Safari/537.36";
   },
   ChromeAdvanced: function ChromeAdvanced(version, WebkitVersion, SysInfo) {
-    return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + ")" + " AppleWebKit/" + WebkitVersion + " (KHTML, like Gecko) Chrome/" + version + " Safari/" + WebkitVersion;
+    return "Mozilla/5.0" + " (" + getSysInfo(SysInfo) + ")" + " AppleWebKit/" + WebkitVersion + " (KHTML, like Gecko) Chrome/" + version + " Safari/" + WebkitVersion;
   },
   firefox: function firefox(version, SysInfo) {
-    return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/20100101 Firefox/" + version;
+    return "Mozilla/5.0" + " (" + getSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/20100101 Firefox/" + version;
   },
   FirefoxAdvanced: function FirefoxAdvanced(version, GeckoVersion, SysInfo) {
-    return "Mozilla/5.0" + " (" + GetSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/" + GeckoVersion + " Firefox/" + version;
+    return "Mozilla/5.0" + " (" + getSysInfo(SysInfo) + "; rv:" + version + ")" + " Gecko/" + GeckoVersion + " Firefox/" + version;
   },
   opera: function opera(version, SysInfo) {
-    return "Opera/9.80 (" + GetSysInfo(SysInfo) + ") Presto/2.12.388 Version/" + version;
+    return "Opera/9.80 (" + getSysInfo(SysInfo) + ") Presto/2.12.388 Version/" + version;
   },
   OperaAdvanced: function OperaAdvanced(version, PrestoVersion, OperaVersion, SysInfo) {
-    return "Opera/" + OperaVersion + " (" + GetSysInfo(SysInfo) + ") Presto/" + PrestoVersion + " Version/" + version;
+    return "Opera/" + OperaVersion + " (" + getSysInfo(SysInfo) + ") Presto/" + PrestoVersion + " Version/" + version;
   },
   safari: function safari(version, SysInfo) {
-    return "Mozilla/5.0 (" + GetSysInfo(SysInfo) + ") AppleWebKit/601.4.8 (KHTML, like Gecko) Version/" + version + " Safari/601.4.8";
+    return "Mozilla/5.0 (" + getSysInfo(SysInfo) + ") AppleWebKit/601.4.8 (KHTML, like Gecko) Version/" + version + " Safari/601.4.8";
   },
   SafariAdvanced: function SafariAdvanced(version, WebkitBuildVersion, SafariBuildVersion, SysInfo) {
-    return "Mozilla/5.0 (" + GetSysInfo(SysInfo) + ") AppleWebKit/" + WebkitBuildVersion + " (KHTML, like Gecko) Version/" + version + " Safari/" + SafariBuildVersion;
+    return "Mozilla/5.0 (" + getSysInfo(SysInfo) + ") AppleWebKit/" + WebkitBuildVersion + " (KHTML, like Gecko) Version/" + version + " Safari/" + SafariBuildVersion;
   }
 };
