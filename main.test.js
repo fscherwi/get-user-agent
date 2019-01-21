7d7acc2187eb1422add4520c0c8bf0a46ee56ef4
fscherwi <fscherwi@outlook.com>
2019-01-21 15:30:27 +0100

    fix lint errors

--- main.test.js
+++ main.test.js
@@ -13,64 +13,64 @@ test('Chrome', t => {
 });
 
 test('Chrome Advanced', t => {
-  var version = '41.0.2228.0';
-  var webkitVersion = '537.36';
-  ua = parser(useragent.chromeAdvanced(version, webkitVersion));
+	const version = '41.0.2228.0';
+	const webkitVersion = '537.36';
+	ua = parser(useragent.chromeAdvanced(version, webkitVersion));
 	t.is(ua.browser.name, 'Chrome');
 	t.is(ua.browser.version, version);
 });
 
 test('Firefox', t => {
-  var version = '41.0';
-  ua = parser(useragent.firefox(version));
+	const version = '41.0';
+	ua = parser(useragent.firefox(version));
 	t.is(ua.browser.name, 'Firefox');
 	t.is(ua.browser.version, version);
 });
 
 test('Firefox Advanced', t => {
-  var version = '41.0';
-  var geckoVersion = '20100101';
-  ua = parser(useragent.firefoxAdvanced(version, geckoVersion));
+	const version = '41.0';
+	const geckoVersion = '20100101';
+	ua = parser(useragent.firefoxAdvanced(version, geckoVersion));
 	t.is(ua.browser.name, 'Firefox');
 	t.is(ua.browser.version, version);
 });
 
 test('Opera', t => {
-  var version = '12.6';
-  ua = parser(useragent.opera(version));
+	const version = '12.6';
+	ua = parser(useragent.opera(version));
 	t.is(ua.browser.name, 'Opera');
 	t.is(ua.browser.version, version);
 });
 
 test('Opera Advanced', t => {
-  var version = '12.6';
-  var prestoVersion = '2.12.388';
-  var operaVersion = '9.80';
-  ua = parser(useragent.operaAdvanced(version, prestoVersion));
+	const version = '12.6';
+	const prestoVersion = '2.12.388';
+	const operaVersion = '9.80';
+	ua = parser(useragent.operaAdvanced(version, prestoVersion, operaVersion));
 	t.is(ua.browser.name, 'Opera');
 	t.is(ua.browser.version, version);
 });
 
 test('Safari', t => {
-  var version = '10.0.3';
-  ua = parser(useragent.safari(version));
+	const version = '10.0.3';
+	ua = parser(useragent.safari(version));
 	t.is(ua.browser.name, 'Safari');
 	t.is(ua.browser.version, version);
 });
 
 test('Safari Advanced', t => {
-  var version = '10.0.3';
-  var webkitBuildVersion = '602.4.8';
-  var safariBuildVersion = '602.4.8';
-  ua = parser(useragent.safariAdvanced(version, webkitBuildVersion, safariBuildVersion));
+	const version = '10.0.3';
+	const webkitBuildVersion = '602.4.8';
+	const safariBuildVersion = '602.4.8';
+	ua = parser(useragent.safariAdvanced(version, webkitBuildVersion, safariBuildVersion));
 	t.is(ua.browser.name, 'Safari');
 	t.is(ua.browser.version, version);
 });
 
 test('sysInfo', t => {
-  var version = '41.0';
-  var sysInfo = 'Macintosh; Intel Mac OS X 10.11';
-  ua = parser(useragent.firefox(version, sysInfo));
+	const version = '41.0';
+	const sysInfo = 'Macintosh; Intel Mac OS X 10.11';
+	ua = parser(useragent.firefox(version, sysInfo));
 	t.is(ua.browser.name, 'Firefox');
 	t.is(ua.browser.version, version);
 });
