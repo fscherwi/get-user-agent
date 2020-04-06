b08c763e597ac4588ff9f7b48d4256afdf4a81ff
fscherwi <fscherwi@outlook.com>
2020-04-06 11:58:45 +0200

    add Typescript types

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+declare const useragent: {
+
+	/**
+	@returns Chrome user agent
+	 */
+	chrome(version: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Chrome user agent with advanced options
+	 */
+	chromeAdvanced(version: string|number, webkitVersion: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Firefox user agent
+	 */
+	firefox(version: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Firefox user agent with advanced options
+	 */
+	firefoxAdvanced(version: string|number, geckoVersion: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Opera user agent
+	 */
+	opera(version: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Opera user agent with advanced options
+	 */
+	operaAdvanced(version: string|number, prestoVersion: string|number, operaVersion: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Safari user agent
+	 */
+	safari(version: string|number, sysInfo?: string): string;
+
+	/**
+	@returns Safari user agent with advanced options
+	 */
+	safariAdvanced(version: string|number, webkitBuildVersion: string|number, safariBuildVersion: string|number, sysInfo?: string): string;
+
+};
+
+export = useragent;
---
+++ index.test-d.ts
@@ -0,0 +1,11 @@
+import {expectType} from 'tsd';
+import getuseragent = require('.');
+
+expectType<string>(getuseragent.chrome('41.0.2228.0'));
+expectType<string>(getuseragent.chromeAdvanced('41.0.2228.0', '537.36'));
+expectType<string>(getuseragent.firefox('41.0'));
+expectType<string>(getuseragent.firefoxAdvanced('41.0', '20100101'));
+expectType<string>(getuseragent.opera('12.6'));
+expectType<string>(getuseragent.operaAdvanced('12.6', '2.12.388', '9.80'));
+expectType<string>(getuseragent.safari('10.0.3'));
+expectType<string>(getuseragent.safariAdvanced('10.0.3', '602.4.8', '602.4.8'));
